91b9e22a3d92b63414eb85deeb20e38d952788db
AlbeMR10 <albertomartelrguez@gmail.com>
2024-11-12 14:04:37 +0000

    Arquitectura finalizada

---
+++ src/software/ulpgc/moneycalculator/architecture/persistence/CurrencyLoader.java
@@ -0,0 +1,8 @@
+package software.ulpgc.moneycalculator.architecture.persistence;
+
+import software.ulpgc.moneycalculator.architecture.model.Currency;
+import java.util.List;
+
+public interface CurrencyLoader {
+    List<Currency> load();
+}
